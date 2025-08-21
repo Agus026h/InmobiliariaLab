dae8bbba508368d1418dfa9e9e57228b0465fb82
Agus026h <agustin201h@gmail.com>
2025-08-21 00:19:44 -0300

    Clase abstracta conexion

---
+++ Models/Conexion.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace devs.Models;
+
+public abstract class Conexion
+{
+    protected readonly IConfiguration configuration;
+    protected readonly String connectionString;
+
+    protected Conexion(IConfiguration configuration)
+    {
+        this.configuration = configuration;
+        connectionString = configuration["ConnectionStrings:DefaultConnection"];
+
+        
+    }
+
+
+}
