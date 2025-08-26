8779442eddd0786d4855075510ad18bf2633e57b
Agus026h <agustin201h@gmail.com>
2025-08-26 01:04:42 -0300

    crear,buscar y modificar p en 1 sola vista

--- Controllers/PropietarioController.cs
+++ Controllers/PropietarioController.cs
@@ -32,8 +32,42 @@ namespace devs.Controllers
         }
 
 
+        
         // POST: Propietario/Crear
         [HttpPost] // para el submit
+
+        public IActionResult Procesar(Propietario p, string accion)
+        {
+            switch (accion)
+            {
+                case "Buscar":
+                    ModelState.Clear();
+                    var persona = _repositorio.buscarId(p.IdPropietario);
+                    if (persona != null)
+                        ViewBag.Mensaje = "Persona encontrada!" + persona.Nombre;
+                        
+                    else
+                        ViewBag.Mensaje = "No se encontro la persona con ese id";
+                    return View("Crear", persona);
+
+                case "Crear":
+
+                    _repositorio.Alta(p);
+                    ViewBag.Mensaje = "Propietario creado con exito";
+                    break;
+
+                case "Modificar":
+
+                    _repositorio.ModificarPropietario(p);
+                    ViewBag.Mensaje = "Propietario modificado con exito";
+                    break;
+           
+            }
+            return View("Crear", p);
+        }
+
+        
+        /*
         [ValidateAntiForgeryToken]
 
         public ActionResult Crear(Propietario p)
@@ -41,10 +75,17 @@ namespace devs.Controllers
             if (ModelState.IsValid)// se fija si el modelo es valido
                 _repositorio.Alta(p);
 
-            return View(p);
+            return View();
         }
 
+       */
 
+        public ActionResult ModificarPropietario(Propietario p)
+        {
+            _repositorio.ModificarPropietario(p);
+            return View();
+
+        }
 
 
     }
--- Models/RepositorioPropietario.cs
+++ Models/RepositorioPropietario.cs
@@ -71,7 +71,7 @@ public class RepositorioPropietario : Conexion
 						Apellido = reader.GetString("Apellido"),
 						Dni = reader.GetString("Dni"),
 						Telefono = reader.GetString("Telefono"),
-						Email = reader.GetString("Telefono"),
+						Email = reader.GetString("Email"),
 
 
 
@@ -86,9 +86,9 @@ public class RepositorioPropietario : Conexion
 
 	}
 
-  
-  
-  public int Baja(Propietario p)
+
+
+	public int Baja(Propietario p)
 	{
 		int res = -1;
 		using (MySqlConnection connection = new MySqlConnection(connectionString))
@@ -103,7 +103,7 @@ public class RepositorioPropietario : Conexion
 				command.CommandType = CommandType.Text;
 				command.Parameters.AddWithValue("idPropietario", p.IdPropietario);
 
-				
+
 				connection.Open();
 
 				res = Convert.ToInt32(command.ExecuteScalar());
@@ -115,4 +115,81 @@ public class RepositorioPropietario : Conexion
 		return res;
 	}
 
-}
+
+
+	public int ModificarPropietario(Propietario p)
+	{
+		int res = 1;
+		using (MySqlConnection connection = new MySqlConnection(connectionString))
+		{
+			String sql = @"UPDATE propietario SET
+			               nombre = @nombre,
+						   apellido = @apellido,
+						   dni = @dni,
+						   telefono = @telefono,
+						   email = @email
+						   WHERE idPropietario = @idPropietario";
+			
+			using (MySqlCommand command = new MySqlCommand(sql, connection))
+			{
+				command.CommandType = CommandType.Text;
+				command.Parameters.AddWithValue("idPropietario", p.IdPropietario);
+				command.Parameters.AddWithValue("nombre", p.Nombre);
+				command.Parameters.AddWithValue("apellido", p.Apellido);
+				command.Parameters.AddWithValue("dni", p.Dni);
+				command.Parameters.AddWithValue("email", p.Email);
+				command.Parameters.AddWithValue("telefono", p.Telefono);
+
+				connection.Open();
+
+				res = Convert.ToInt32(command.ExecuteScalar());
+
+				p.IdPropietario = res;
+				connection.Close();
+			}
+
+			return res;
+		}
+
+
+	 }
+
+	public Propietario buscarId(int id)
+	{
+		Propietario p = null;
+		using (MySqlConnection connection = new MySqlConnection(connectionString))
+		{
+         string sql = @"SELECT IdPropietario, Nombre, Apellido, Dni, Telefono, Email 
+					FROM Propietario
+					WHERE IdPropietario = @idPropietario";
+			using (MySqlCommand command = new MySqlCommand(sql, connection))
+			{
+				command.Parameters.AddWithValue("@idPropietario", id);
+					command.CommandType = CommandType.Text;
+					connection.Open();
+					var reader = command.ExecuteReader();
+					if (reader.Read())
+					{
+						p = new Propietario
+						{
+							IdPropietario = reader.GetInt32(nameof(Propietario.IdPropietario)),
+							Nombre = reader.GetString("Nombre"),
+							Apellido = reader.GetString("Apellido"),
+							Dni = reader.GetString("Dni"),
+							Telefono = reader.GetString("Telefono"),
+							Email = reader.GetString("Email"),
+							
+						};
+					}
+					connection.Close();
+				}
+			}
+			return p;
+
+
+		}
+
+
+	}
+
+
